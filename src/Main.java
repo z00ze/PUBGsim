c634c4225f542d32ae633b55729f833a6d74a394
Marko Loponen <mmmarko@live.com>
2018-09-25 23:33:29 +0300

    centerzone calculator

--- src/Main.java
+++ src/Main.java
@@ -62,6 +62,7 @@ public class Main extends Application {
         LinkedList<Circle> safetyzones = new LinkedList<>();
         LinkedList<Event> events = main.getEvents();
         for(Event event:events){
+            if(event.getCommon().getIsGame() == 0) continue;
             Color color = Color.valueOf(colors[(int) event.getCommon().getIsGame()]);
             int x = (Math.round(event.getLocation().getX()) / 1000);
             int y = (Math.round(event.getLocation().getY()) / 1000);
@@ -72,7 +73,7 @@ public class Main extends Application {
             }
 
             // Draw just safetyzones on stable phases (0.5, 1.5, ... n.5 = moving safetyzone)
-            if(event.getEventType() == Event.EventType.LogGameStatePeriodic && event.getCommon().getIsGame() % 1 == 0){
+            if(event.getEventType() == Event.EventType.LogGameStatePeriodic && event.getCommon().getIsGame() % 1 == 0){ // && event.getCommon().getIsGame() >= 0.5
                 Boolean add = true;
                 Circle circle = new Circle(event.getLogGameStatePeriodic().getGameState().getSafetyZonePosition().getX(),event.getLogGameStatePeriodic().getGameState().getSafetyZonePosition().getY()+toolbarHeight,event.getLogGameStatePeriodic().getGameState().getSafetyZoneRadius());
                 for(Circle c:safetyzones){
@@ -86,7 +87,6 @@ public class Main extends Application {
                     circle.setFill(Color.TRANSPARENT);
                     circle.setStrokeWidth(1);
                     circle.setId(event.getCommon().getIsGame()+"");
-                    System.out.println(circle.toString());
                     safetyzones.add(circle);
                 }
             }
@@ -96,11 +96,10 @@ public class Main extends Application {
         for(Event event:events){
             for(Circle safetyzone:safetyzones){
                 if(event.getEventType() == Event.EventType.LogPlayerKill && Double.parseDouble(safetyzone.getId()) == event.getCommon().getIsGame()){
-                    event.getLogPlayerKill().setSafezone(safetyzone);
+                        event.getLogPlayerKill().setSafezone(safetyzone);
                 }
             }
         }
-
 
         //Setting the image view
         ImageView imageView = new ImageView(wImage);
@@ -221,17 +220,27 @@ public class Main extends Application {
         //Displaying the contents of the stage
         stage.show();
 
+        LinkedList<Double> deathDistances = new LinkedList<>();
         // Death calculating
         for(Event event:events){
-            if(event.getEventType() == Event.EventType.LogPlayerKill){
-                System.out.println(distance(
+            if(event.getEventType() == Event.EventType.LogPlayerKill){ //  && event.getCommon().getIsGame() % 1 == 0
+                if(event.getLogPlayerKill().getSafezone() == null || event.getLogPlayerKill().getSafezone() == null) continue;
+                deathDistances.add(distance(
                         event.getLogPlayerKill().getVictim().getLocation().getX(),
                         event.getLogPlayerKill().getVictim().getLocation().getY(),
                         event.getLogPlayerKill().getSafezone().getCenterX(),
                         event.getLogPlayerKill().getSafezone().getCenterY()
-                ));
+                ) / event.getLogPlayerKill().getSafezone().getRadius());
             }
         }
+        int centerSafetyzoneDeathAmount = 0;
+        int outerSafetyzoneDeathAmount = 0;
+        for(Double d:deathDistances){
+            if(d > 0.5) outerSafetyzoneDeathAmount++;
+            else centerSafetyzoneDeathAmount++;
+        }
+        System.out.println("centerSafetyzoneDeathAmount : " + centerSafetyzoneDeathAmount);
+        System.out.println("outerSafetyzoneDeathAmount : " + outerSafetyzoneDeathAmount);
     }
     public static void main(String args[]) {
         launch(args);
